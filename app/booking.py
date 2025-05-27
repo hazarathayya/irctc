328c6a5fd7451282f034412309e2ed584ec0f29f
Hazarath <hazarathayyayallanki@gmail.com>
2025-05-27 15:54:46 +0530

    updated

--- app/booking.py
+++ app/booking.py
@@ -5,33 +5,29 @@ from .routes import api_blueprint
 
 # Book a seat
 @api_blueprint.route('/book', methods=['POST'])
+@token_required
 def book_seat():
     data = request.get_json()
+    
+    if not data.get('train_id'):
+        return jsonify({'message': 'Missing train_id!'}), 400
 
-    # Check for required data
-    if not data.get('user_id') or not data.get('train_id'):
-        return jsonify({'message': 'Missing user_id or train_id!'}), 400
-
-    # Find the user and the train
-    user = User.query.get(data['user_id'])
     train = Train.query.get(data['train_id'])
 
-    if not user or not train:
-        return jsonify({'message': 'Invalid user or train!'}), 404
+    if not train:
+        return jsonify({'message': 'Invalid train!'}), 404
 
-    # Check if seats are available
     if train.available_seats <= 0:
         return jsonify({'message': 'No available seats!'}), 400
 
-    # Create a new booking
-    new_booking = Booking(user_id=user.id, train_id=train.id)
+    new_booking = Booking(user_id=g.user['id'], train_id=train.id)
     db.session.add(new_booking)
 
-    # Decrement available seats
     train.available_seats -= 1
     db.session.commit()
 
     return jsonify({'message': 'Seat booked successfully!'}), 201
+
 
 # Get specific booking details
 @api_blueprint.route('/booking/<int:booking_id>', methods=['GET'])
